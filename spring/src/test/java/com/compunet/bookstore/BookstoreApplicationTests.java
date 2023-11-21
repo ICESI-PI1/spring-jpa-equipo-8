a88b6b1dd86650019102837bb631b1d86259fbb2
Samhernesp <samhernesp@gmail.com>
2023-11-21 11:51:18 -0500

    All test completed

--- spring/src/test/java/com/compunet/bookstore/BookstoreApplicationTests.java
+++ spring/src/test/java/com/compunet/bookstore/BookstoreApplicationTests.java
@@ -16,8 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -36,7 +35,7 @@ class BookstoreApplicationTests {
 	private IBookService bookService;
 
 	@Test
-	public void whenGetAllAuthor_thenAuthorListShouldBeReturned() {
+	public void getAllAuthorsTest() {
 		List<Author> expectedAuthors = Arrays.asList(new Author(1L, "Autor1", "Nacionalidad1"), new Author(2L, "Autor2", "Nacionalidad2"));
 		when(autorRepository.findAllByOrderByNombreAsc()).thenReturn(expectedAuthors);
 
@@ -46,7 +45,7 @@ class BookstoreApplicationTests {
 	}
 
 	@Test
-	public void whenGetDetails_withValidId_thenAuthorShouldBeReturned() {
+	public void getAuthorDetailsTest() {
 		Author expectedAuthor = new Author(1L, "Autor1", "Nacionalidad1");
 		when(autorRepository.findById(1L)).thenReturn(Optional.of(expectedAuthor));
 
@@ -57,7 +56,7 @@ class BookstoreApplicationTests {
 	}
 
 	@Test
-	public void whenCreateAuthor_thenAuthorShouldBeSaved() {
+	public void createAuthorTest() {
 		Author newAuthor = new Author(5L, "Nuevo Autor", "Nacionalidad");
 
 		autorService.createAutor(newAuthor);
@@ -65,10 +64,45 @@ class BookstoreApplicationTests {
 		verify(autorRepository).save(newAuthor);
 	}
 
-	//Book Test
+	@Test
+	public void editAuthorTest() {
+		Author existingAuthor = new Author(1L, "Autor Original", "Nacionalidad Original");
+
+		autorService.createAutor(existingAuthor);
+
+		Author updatedAuthor = new Author(1L, "Autor Modificado", "Nacionalidad Modificada");
+		when(autorRepository.save(updatedAuthor)).thenReturn(updatedAuthor);
+
+		when(autorRepository.findById(1L)).thenReturn(Optional.of(updatedAuthor));
+
+		autorService.editAutor(updatedAuthor);
+
+		verify(autorRepository).save(updatedAuthor);
+
+		Author author = autorService.getDetails(1L).get();
+
+		assertEquals(updatedAuthor,author);
+
+	}
 
 	@Test
-	public void whenGetBookByAuthor_withValidAuthorId_thenBookListShouldBeReturned() {
+	public void deleteAuthorTest() {
+		Author newAuthor = new Author(5L, "Nuevo Autor", "Nacionalidad");
+
+		autorService.createAutor(newAuthor);
+
+		Long authorId = 5L;
+
+		autorService.deleteAutor(authorId);
+
+		verify(autorRepository).deleteById(authorId);
+
+		assertEquals(autorService.getDetails(authorId),Optional.empty());
+
+	}
+
+	@Test
+	public void getBookByAuthorTest() {
 		Author autor = new Author(1L, "Autor1", "Nacionalidad1");
 		List<Book> expectedBooks = Arrays.asList(new Book(1L, "Libro1", new Date(), autor));
 		when(autorRepository.getBookByAutor(1L)).thenReturn(expectedBooks);
@@ -78,8 +112,10 @@ class BookstoreApplicationTests {
 		assertEquals(expectedBooks, actualBooks);
 	}
 
+
+
 	@Test
-	public void whenSaveBook_thenBookShouldBeSaved() {
+	public void createBooksTest() {
 		Book newBook = new Book(null, "Nuevo Libro", new Date(), new Author(1L, "Autor1", "Nacionalidad1"));
 
 		bookService.save(newBook);
@@ -88,7 +124,7 @@ class BookstoreApplicationTests {
 	}
 
 	@Test
-	public void whenFindById_withValidId_thenBookShouldBeReturned() {
+	public void getBookDetailsTest() {
 		Book expectedBook = new Book(1L, "Libro1", new Date(), new Author(1L, "Autor1", "Nacionalidad1"));
 		when(bookRepository.findById(1L)).thenReturn(Optional.of(expectedBook));
 
@@ -98,8 +134,54 @@ class BookstoreApplicationTests {
 		assertEquals(expectedBook, actualBook.get());
 	}
 
+	@Test
+	public void editBookTest() {
+		Book bookToUpdate = new Book(1L, "Libro", new Date(), new Author(1L, "Autor 1", "Nacionalidad 1"));
 
+		bookService.save(bookToUpdate);
 
+		Book updatedBook = new Book(1L, "Libro Actualizado", new Date(), new Author(1L, "Autor 2", "Nacionalidad 2"));
+		when(bookRepository.save(updatedBook)).thenReturn(updatedBook);
+
+		when(bookRepository.findById(1L)).thenReturn(Optional.of(updatedBook));
+
+		bookService.edit(updatedBook);
+
+		verify(bookRepository).save(updatedBook);
+
+		Book book = bookService.findById(1L).get();
+
+		assertEquals(updatedBook,book);
+	}
+
+	@Test
+	public void deleteBookTest() {
+		Book expectedBook = new Book(5L, "Libro1", new Date(), new Author(1L, "Autor1", "Nacionalidad1"));
+
+		bookService.save(expectedBook);
+
+		Long bookId = 5L;
+
+		bookService.deleteById(bookId);
+
+		verify(bookRepository).deleteById(bookId);
+
+		assertEquals(bookService.findById(bookId),Optional.empty());
+
+	}
+
+	@Test
+	public void getAllBooksTest() {
+		List<Book> expectedBooks = Arrays.asList(
+				new Book(1L, "Libro 1", new Date(), new Author(1L, "Autor 1", "Nacionalidad 1")),
+				new Book(2L, "Libro 2", new Date(), new Author(2L, "Autor 2", "Nacionalidad 2"))
+		);
+		when(bookRepository.findAllByOrderByTitleAsc()).thenReturn(expectedBooks);
+
+		List<Book> actualBooks = bookService.getAllBook();
+
+		assertEquals(expectedBooks, actualBooks);
+	}
 
 
 }
